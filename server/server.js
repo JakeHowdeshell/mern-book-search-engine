616f8b2abeac6a07c7a67e02c0ab20079bec4c49
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-11-13 18:52:47 -0600

    un commented the production code

--- server/server.js
+++ server/server.js
@@ -24,20 +24,20 @@ const startApolloServer = async () => {
   app.use(
     "/graphql",
     expressMiddleware(server, {
-      context: {},
+      context: authMiddleware,
     })
   );
 
   // if we're in production, serve client/build as static assets
-  // if (process.env.NODE_ENV === "production") {
-  //   app.use(express.static(path.join(__dirname, "../client/dist")));
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
 
-  //   app.get("*", (req, res) => {
-  //     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-  //   });
-  // }
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+  }
 
-  // app.use(routes);
+  app.use(routes);
 
   db.once("open", () => {
     app.listen(PORT, () => {
