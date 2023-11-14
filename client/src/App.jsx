4b67320b364a5eaf494b381e5298b845b4ebd37d
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-11-13 18:35:12 -0600

    texted different links and returned to previous structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,37 @@
-import './App.css';
-import { setContext } from '@apollo/client/link/context';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import { setContext } from "@apollo/client/link/context";
+import { Outlet } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
+} from "@apollo/client";
 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 // Construct the main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
+
+  console.log("token", token);
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
 
 const client = new ApolloClient({
   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  // link: from([errorLink, authLink, httpLink]),
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
@@ -38,7 +41,7 @@ function App() {
     <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-      </ApolloProvider>
+    </ApolloProvider>
   );
 }
 
